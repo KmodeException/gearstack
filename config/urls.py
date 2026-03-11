0953d603ca3b79af614e4824b885077fcf507104
KmodeException <luismansillatrab@gmail.com>
2026-03-11 04:22:39 -0300

    feat: implementar vista de lista de inventario y estructura de templates

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from inventory.views import item_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', item_list, name='item_list'),
 ]
